93ebc7b624bb358add297972b0f05fda46cc9845
SimonaStoeva <s22621604@onlineedu.tu-varna.bg>
2025-08-02 22:00:08 +0300

    Initial commit

--- laps_per_driver.py
+++ laps_per_driver.py
@@ -8,6 +8,7 @@ fastf1.Cache.enable_cache('D:\\Me\\idk\\python\\F1-data')
 session = fastf1.get_session(2024, 12, "R")
 session.load()
 
+
 laps = session.laps
 ham_laps = laps.pick_drivers(['HAM'])
 ham_laps['LapTimeSeconds'] = ham_laps['LapTime'].dt.total_seconds()
