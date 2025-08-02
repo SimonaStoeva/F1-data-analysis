6b6dbd4d0b768fb7db34e6be5aaa65cf15b47234
SimonaStoeva <s22621604@onlineedu.tu-varna.bg>
2025-08-02 21:56:01 +0300

    Initial commit

---
+++ laps_per_driver.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
+import fastf1
+
+fastf1.Cache.enable_cache('D:\\Me\\idk\\python\\F1-data')
+session = fastf1.get_session(2024, 12, "R")
+session.load()
+
+laps = session.laps
+ham_laps = laps.pick_drivers(['HAM'])
+ham_laps['LapTimeSeconds'] = ham_laps['LapTime'].dt.total_seconds()
+
+plt.plot(ham_laps['LapNumber'], ham_laps['LapTimeSeconds'], marker='o')
+plt.title("Hamilton Lap Times")
+plt.xlabel("Lap Number")
+plt.ylabel("Time (s)")
+plt.show()
